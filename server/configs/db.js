7682c74621212bc6f84f0b633a66228df3bc2bf9
Aishwarya S <88452930+AishwaryaS9@users.noreply.github.com>
2025-06-11 11:55:27 +0530

    Updated db.js

--- server/configs/db.js
+++ server/configs/db.js
@@ -2,9 +2,9 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
     try {
-        // await mongoose.connect(`${process.env.MONGO_URI}/wanderNest`)
         mongoose.connection.on('connected', () => console.log('Database Connected'));
-        await mongoose.connect(`${process.env.MONGO_URI}`)
+        //await mongoose.connect(`${process.env.MONGO_URI}`)
+        await mongoose.connect(`${process.env.MONGO_URI}/wanderNest`)
 
     } catch (error) {
         console.log(error.message);
