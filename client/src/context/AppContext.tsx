2ee46ef4bbb78a0f06031d28ed5e48488f3343ff
AishwaryaS9 <aishwaryasudupi9@gmail.com>
2025-06-12 20:04:56 +0530

    Integrated apis for Dashboard and Search

--- client/src/context/AppContext.tsx
+++ client/src/context/AppContext.tsx
@@ -13,12 +13,24 @@ export const AppProvider = ({ children }) => {
     const navigate = useNavigate();
     const { user } = useUser();
     const { getToken } = useAuth();
-    // const token =  getToken();
-    // console.log("Token:", token);
 
     const [isOwner, setIsOwner] = useState(false);
     const [showHotelReg, setShowHotelReg] = useState(false);
     const [searchedCities, setSearchedCities] = useState([]);
+    const [rooms, setRooms] = useState([]);
+
+    const fetchRooms = async () => {
+        try {
+            const {data} = await axios.get('/api/rooms')
+            if (data.success) {
+                setRooms(data.rooms)
+            } else {
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
 
     const fetchUser = async () => {
         try {
@@ -40,18 +52,19 @@ export const AppProvider = ({ children }) => {
         }
     }
 
-
-
-
     useEffect(() => {
         if (user) {
             fetchUser();
         }
-    }, [user])
+    }, [user]);
+
+    useEffect(() => {
+        fetchRooms();
+    }, [])
 
     const value = {
         currency, navigate, user, getToken, isOwner, setIsOwner,
-        axios, showHotelReg, setShowHotelReg, searchedCities, setSearchedCities
+        axios, showHotelReg, setShowHotelReg, searchedCities, setSearchedCities, rooms, setRooms
     }
 
     return (
--- client/src/pages/hotelOwner/ListRoom.tsx
+++ client/src/pages/hotelOwner/ListRoom.tsx
@@ -1,9 +1,50 @@
-import { useState } from 'react'
-import { roomsDummyData } from '../../assets/assets'
+import { useEffect, useState } from 'react'
 import Title from '../../components/Title';
+import { useAppContext } from '../../context/AppContext';
+import toast from 'react-hot-toast';
 
 const ListRoom = () => {
-  const [rooms, setRooms] = useState(roomsDummyData);
+  const [rooms, setRooms] = useState([]);
+
+  const { axios, getToken, user, currency } = useAppContext();
+
+  const fetchRooms = async () => {
+    try {
+      const { data } = await axios.get('/api/rooms/owner', {
+        headers: {
+          Authorization: `Bearer ${await getToken()}`
+        }
+      });
+      if (data.success) {
+        setRooms(data.rooms)
+      } else {
+        toast.error(data.message)
+      }
+
+    } catch (error) {
+      toast.error(error.message)
+    }
+  }
+
+  const toggleAvailability = async (roomId) => {
+    const { data } = await axios.post('/api/rooms/toggle-availability', { roomId }, {
+      headers: {
+        Authorization: `Bearer ${await getToken()}`
+      }
+    })
+    if (data.success) {
+      toast.success(data.message)
+      fetchRooms();
+    } else {
+      toast.error(data.message)
+    }
+  }
+
+  useEffect(() => {
+    if (user) {
+      fetchRooms();
+    }
+  }, [user]);
 
   return (
     <div>
@@ -32,11 +73,11 @@ const ListRoom = () => {
                   {item.amenities.join(', ')}
                 </td>
                 <td className='py-3 px-4 text-gray-700 border-t border-gray-300'>
-                  {item.pricePerNight}
+                  {currency} {item.pricePerNight}
                 </td>
                 <td className='py-3 px-4 border-t border-gray-300 text-sm text-red-500 text-center'>
                   <label className='relative inline-flex items-center cursor-pointer text-gray-900 gap-3'>
-                    <input type="checkbox"
+                    <input type="checkbox" onChange={() => toggleAvailability(item._id)}
                       className='sr-only peer' checked={item.isAvailable} />
                     <div className='w-12 h-7 bg-slate-300 rounded-full peer peer-checked:bg-blue-600
                     transition-colors duration-200'></div>
