a6a00cc47b029fb70cfb0769e8fbc432a5a7e121
Aishwarya S <88452930+AishwaryaS9@users.noreply.github.com>
2025-06-14 19:37:17 +0530

    Updated db.js

--- server/configs/db.js
+++ server/configs/db.js
@@ -3,8 +3,8 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
     try {
         mongoose.connection.on('connected', () => console.log('Database Connected'));
-        //await mongoose.connect(`${process.env.MONGO_URI}`)
-        await mongoose.connect(`${process.env.MONGO_URI}/wanderNest`)
+        await mongoose.connect(`${process.env.MONGO_URI}`)
+        // await mongoose.connect(`mongodb+srv://aishwaryas0908:aish*3927@mydbdata.19qlr.mongodb.net/wanderNest?retryWrites=true&w=majority`)
 
     } catch (error) {
         console.log(error.message);
